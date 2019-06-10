02a650795561ed88c57f89078e42628377e9380d
Adam.Ouellette <ouellette.a@gmail.com>
2019-06-10 13:38:09 -0400

    Made testing changes to len of first and last name

---
+++ WorkingWithInput/TestIfStatement.py
@@ -0,0 +1,8 @@
+Data = int(input("Input a number greater than 3 and less than 50: "))
+# print(len(Data))
+if Data < 3:
+    print(f"Value {Data} is less than 3")
+elif Data > 50:
+    print(f"Value {Data} is greater than 50")
+else:
+    print(f"Your value {Data} is perfect!!")
