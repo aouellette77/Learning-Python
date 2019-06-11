b2afd844991205d3db77530b02b99d9906c09d22
Adam Ouellette <ouellette.a@gmail.com>
2019-06-11 09:01:40 -0400

    Adding .idea and WorkingWithInput.py files

---
+++ WorkingWithInput/MultiLineString.py
@@ -0,0 +1,10 @@
+name = "Adam"
+profession = "IT Manager"
+affiliation = "Matrix"
+
+message = (
+    f"Hi {name}. "
+    f"You are a {profession}. "
+    f"You were in the {affiliation}."
+    )
+print(message)
